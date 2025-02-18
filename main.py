d2d3d3b7bd10a5441963dda0046a402e90605e1b
kenochou <2078661854@qq.com>
2025-02-18 15:41:40 +0800

    导入模块

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-try:
-    num = int(input("输入数字："))
-except ValueError:
-    print("输入无效！")
-else:
-    print(f"输入的数字是：{num}")
-finally:
-    print("处理结束。")
+import math
+print(math.sqrt(16))  # 4.0
+
+# 别名导入
+import numpy as np
