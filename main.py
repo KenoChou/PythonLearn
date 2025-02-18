8e4553bbd3dd623ed4c9a6789c7f37f941ac4adc
kenochou <2078661854@qq.com>
2025-02-18 16:36:05 +0800

    元类的基本用法

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-# 类似列表推导式，但返回生成器对象
-squares = (x**2 for x in range(10))
-print(next(squares))  # 0
-print(next(squares))  # 1
+# 自定义一个元类：强制类属性名大写
+class UpperAttrMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        # 遍历属性，将非特殊属性名转为大写
+        uppercase_attrs = {
+            attr.upper() if not attr.startswith("__") else attr: value
+            for attr, value in attrs.items()
+        }
+        return super().__new__(cls, name, bases, uppercase_attrs)
+
+# 使用元类
+class MyClass(metaclass=UpperAttrMetaclass):
+    value = 10
+
+print(MyClass.VALUE)  # 输出: 10（属性名被自动转为大写）
