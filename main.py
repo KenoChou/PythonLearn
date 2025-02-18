5bd144c013bc56e03235ca2964a41af9b10bb93e
kenochou <2078661854@qq.com>
2025-02-18 14:38:13 +0800

    while循环

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-for i in range(5):     # 输出0-4
-    print(i)
-
-fruits = ["苹果", "香蕉", "橘子"]
-for fruit in fruits:   # 遍历列表
-    print(fruit)
+count = 0
+while count < 3:
+    print(count)
+    count += 1
