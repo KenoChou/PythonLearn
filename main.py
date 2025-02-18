8a7fe9ea16a3609e63b5be4e28e4e96bb4216370
kenochou <2078661854@qq.com>
2025-02-18 15:41:12 +0800

    异常处理

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-class Animal:
-    # 构造方法
-    def __init__(self, name):
-        self.name = name
-    
-    def bark(self,call):
-        print(f"{self.name}：{call}！")
-    def skill(self):
-        print(f"{self.name}：我会吃饭！")
-
-my_dog = Animal("旺财")
-my_dog.bark("汪汪")  # 输出：旺财：汪汪！
-class Cat(Animal):  # 继承Dog类
-    def bark(self,call):
-        print(f"{self.name}：{call}")
-
-my_cat = Cat("小白")
-my_cat.bark("喵喵")  # 输出：小白：喵喵！
-my_cat.skill()  # 输出：小白：我会吃饭！
+try:
+    num = int(input("输入数字："))
+except ValueError:
+    print("输入无效！")
+else:
+    print(f"输入的数字是：{num}")
+finally:
+    print("处理结束。")
